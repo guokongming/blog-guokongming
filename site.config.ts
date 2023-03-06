0d79da7c9faddc4126eb60044c2934f5ef5b2e4e
郭孔明 <55223551+guokongming@users.noreply.github.com>
2023-03-06 15:33:03 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: '郭孔明律师Notion博客',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: '郭孔明',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
