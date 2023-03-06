0fc1184421fc321df75171ecddfb1f465ed37b51
郭孔明 <55223551+guokongming@users.noreply.github.com>
2023-03-06 16:02:26 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,9 +18,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'guokongming',
+  github: 'guokongming',
+  linkedin: 'guokongming',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
