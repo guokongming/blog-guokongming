aa67ad201416bd80c79d5a9c60e79efb9b2823e8
郭孔明 <55223551+guokongming@users.noreply.github.com>
2023-03-06 16:40:24 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,16 +51,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+ navigationStyle: 'custom',
+ navigationLinks: [
+ {
+     title: 'About',
+   pageId: '957ed75f380d4f97952b36fcb73cbbce'
+   },
+  {
+  title: 'Contact',
+      pageId: '372dc64d97a74f0db19506b4a68cc1cc'
+    }
+   ]
 })
